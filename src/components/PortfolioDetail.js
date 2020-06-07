0c4e5f93fb367444b4bdd9cf6ee36f607207ee11
yugamgangar <gangaryugam@gmail.com>
2020-06-07 16:51:31 +0530

    packages updated

--- src/components/PortfolioDetail.js
+++ src/components/PortfolioDetail.js
@@ -1,38 +1,73 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Carousel } from 'react-bootstrap'
 import Footer from './Footer'
-import Header from './Header'
+import { animateScroll as scroll } from 'react-scroll'
+import { Link } from 'react-router-dom'
+
+let copyData = null
 
 const PortfolioDetail = props => {
+  const [data, setData] = useState({
+    path: '',
+    id: [],
+    title: '',
+    des: '',
+    stack: ''
+  })
+
+  useEffect(() => {
+    getData()
+    copyData = data
+  }, [])
+
+  const getData = () => {
+    props.location.state !== undefined
+      ? setData(props.location.state)
+      : setData(copyData)
+  }
+
   const carousel = () => {
-    const ids = props.location.state.id
+    const ids = data.id
     return (
       <Carousel interval={null} touch>
-        {
-          ids.map(item => {
-            return (
-              <Carousel.Item key={item}>
-                <img className='d-block w-100 img-fluid' id={item}
-                  alt='First slide' />
-              </Carousel.Item>
-            )
-          })
-        }
+        {ids.map(item => {
+          return (
+            <Carousel.Item key={item}>
+              <img
+                className='d-block w-100 img-fluid'
+                id={item}
+                alt='First slide'
+              />
+            </Carousel.Item>
+          )
+        })}
       </Carousel>
     )
   }
 
+  console.log(data)
+
+  useEffect(() => {
+    scroll.scrollToTop()
+  }, [])
+
   return (
     <div className='portfolioDetail-section'>
-      <Header windowWidth={900} />
+      <div className='w-100 justify-content-center row align-items-center'>
+        <Link to='/'>
+          <img
+            src={require('../assets/logo.svg')}
+            className='portfolio-header-logo'
+            alt='Home'
+          />
+        </Link>
+      </div>
       <div className='pfd-body d-flex'>
         <div className='container pfd-container my-5'>
-          <div className='carousel-container my-5'>
-            {carousel()}
-          </div>
-          <p className='project-title'>{props.location.state.title}</p>
-          <p className='project-description'>{props.location.state.des}</p>
-          <p className='project-stack'>{props.location.state.stack}</p>
+          <div className='carousel-container my-5'>{carousel()}</div>
+          <p className='project-title'>{data.title}</p>
+          <p className='project-description'>{data.des}</p>
+          <p className='project-stack'>{data.stack}</p>
         </div>
       </div>
       <Footer />
